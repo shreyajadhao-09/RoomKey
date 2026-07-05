04cae663a75c6e943649f96d03fac336f0bae807
Shreya Jadhao <shreyajadhao2004@gmail.com>
2026-07-05 12:41:15 +0530

    Added signUp feature

--- app.js
+++ app.js
@@ -11,10 +11,14 @@ const ExpressError = require("./utils/ExpressError.js");
 // const Review = require("./models/review.js")
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local")
+const User = require("./models/user.js");
 
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -57,22 +61,42 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next)=>{
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 }); 
 
-  //routes
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+// app.get("/demouser", async(req, res)=>{
+//   let fakeUser = new User({
+//     email:"student@gmail.com",
+//     username:"delta-student",
+//   });
 
-app.all("/{*splat}", (req, res, next) => {
+//   let registerUser = await User.register(fakeUser, "helloworld");
+//   res.send(registerUser);
+// });
+
+//routes
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
+
+app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
 });
+
 //Middelware: errorHandling
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something Went Wrong!" } = err;
   // res.status(statusCode).send(message);
   res.status(statusCode).render("error.ejs", { err })
 });
+
+// /pbkdf2 hashing fun 
