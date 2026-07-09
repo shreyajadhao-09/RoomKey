05e75e3e993c9be03c066859e06e495695ebd921
Shreya Jadhao <shreyajadhao2004@gmail.com>
2026-07-10 00:08:31 +0530

    Update seed script for Atlas

--- init/index.js
+++ init/index.js
@@ -2,26 +2,42 @@ const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
 
+
+const dns = require("dns");
+require("dotenv").config();
+
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+// main()
+//   .then(() => {
+//     console.log("cuection succesfully");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
 main()
   .then(() => {
-    console.log("cuection succesfully");
+    console.log("connection successfully");
+    return initDB();
   })
   .catch((err) => {
     console.log(err);
   });
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+  // await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+  await mongoose.connect(process.env.ATLASDB_URL);
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
   initdata.data = initdata.data.map((obj) => ({
     ...obj,
-    owner: "6a4a4ee2112fe24951a0026a",
+    owner: "6a4faefb9232db5c0f1cb146",
   }));
   await Listing.insertMany(initdata.data);
   console.log("Data was initialized");
 };
 
-initDB();
+// initDB();
